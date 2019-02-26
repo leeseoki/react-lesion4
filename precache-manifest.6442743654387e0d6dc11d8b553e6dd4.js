22c5f4e3ed39d231d288a010289679202b8472ac
Pham Thi Van <phamthivan@Vans-Mac.local>
2019-02-26 15:35:29 +0700

    Updates

---
+++ precache-manifest.6442743654387e0d6dc11d8b553e6dd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "574c35fb47011f8c3327",
+    "url": "/leeseoki/react-lesion4/static/js/runtime~main.574c35fb.js"
+  },
+  {
+    "revision": "a3dee77b7c45cd50bbe3",
+    "url": "/leeseoki/react-lesion4/static/js/main.a3dee77b.chunk.js"
+  },
+  {
+    "revision": "fd5b5b556f35f799451a",
+    "url": "/leeseoki/react-lesion4/static/js/2.fd5b5b55.chunk.js"
+  },
+  {
+    "revision": "a3dee77b7c45cd50bbe3",
+    "url": "/leeseoki/react-lesion4/static/css/main.c1244b19.chunk.css"
+  },
+  {
+    "revision": "cba9b9995f94b8a7364a078ce06283a0",
+    "url": "/leeseoki/react-lesion4/index.html"
+  }
+];
